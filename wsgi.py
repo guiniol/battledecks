f4fc194576ec91177226a349f7c611ee5f989a8f
Guillaume Brogi <gui-gui@netcourrier.com>
2016-12-30 10:52:48 +0100

    Rename app file to battledecks

--- wsgi.py
+++ wsgi.py
@@ -12,7 +12,7 @@ except IOError:
 # line, it's possible required libraries won't be in your searchable path
 #
 
-from flaskapp import app as application
+from battledecks import app as application
 
 #
 # Below for testing only
