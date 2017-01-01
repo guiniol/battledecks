50f3805e61a36b77c82e7d44e99aadc0dbe6aa37
Guillaume Brogi <gui-gui@netcourrier.com>
2017-01-01 17:50:29 +0100

    Remove margin when hiding

--- static/script.js
+++ static/script.js
@@ -1,61 +1,61 @@
 function show(e) {
-	var c = e.children;
-	if (c.length > 0) {
-		c[0].style.display = "inline";
-	} else {
-		var image = e.attributes.oli.value;
-		var img = document.createElement("img");
-		img.src = image;
-		img.className = "popupimage";
-		e.appendChild(img);
-	}
+    var c = e.children;
+    if (c.length > 0) {
+        c[0].style.display = "inline";
+    } else {
+        var image = e.attributes.oli.value;
+        var img = document.createElement("img");
+        img.src = image;
+        img.className = "popupimage";
+        e.appendChild(img);
+    }
 }
 
 function hide(e) {
-	var c = e.children;
-	if (c.length > 0) {
-		c[0].style.display = "none";
-	}
+    var c = e.children;
+    if (c.length > 0) {
+        c[0].style.display = "none";
+    }
 }
 
 function toggle(e) {
-	if (e.classList.contains("selectit")) {
-		e.classList.remove("selectit");
-		e.classList.add("avoidit");
-	} else if (e.classList.contains("avoidit")) {
-		e.classList.remove("avoidit");
-	} else {
-		e.classList.add("selectit");
-	}
-	update_filter(e.parentElement);
+    if (e.classList.contains("selectit")) {
+        e.classList.remove("selectit");
+        e.classList.add("avoidit");
+    } else if (e.classList.contains("avoidit")) {
+        e.classList.remove("avoidit");
+    } else {
+        e.classList.add("selectit");
+    }
+    update_filter(e.parentElement);
 }
 
 function update_filter(e) {
-	var elems = document.getElementsByClassName("deck");
-	var childs = e.children;
-	var showit = [];
-	var hideit = [];
+    var elems = document.getElementsByClassName("deck");
+    var childs = e.children;
+    var showit = [];
+    var hideit = [];
     var combine = document.getElementById("manaCB").checked;
-	for (var c = 0; c < childs.length; ++c) {
-		if (childs[c].classList.contains("selectit")) {
-			showit.push(childs[c].id)
-		} else if (childs[c].classList.contains("avoidit")) {
-			hideit.push(childs[c].id)
-		}
-	}
-	for (var idx = 0; idx < elems.length; ++idx) {
-		var dc = elems[idx].attributes.dc.value;
-		var found = false;
-		for (var i = 0; i < hideit.length; ++i) {
-			if (dc.indexOf(hideit[i]) > -1) {
-				found = true;
-				break;
-			}
-		}
-		if (found) {
-			elems[idx].style.display = "none";
-			continue;
-		}
+    for (var c = 0; c < childs.length; ++c) {
+        if (childs[c].classList.contains("selectit")) {
+            showit.push(childs[c].id)
+        } else if (childs[c].classList.contains("avoidit")) {
+            hideit.push(childs[c].id)
+        }
+    }
+    for (var idx = 0; idx < elems.length; ++idx) {
+        var dc = elems[idx].attributes.dc.value;
+        var found = false;
+        for (var i = 0; i < hideit.length; ++i) {
+            if (dc.indexOf(hideit[i]) > -1) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            elems[idx].classList.add("hideme");
+            continue;
+        }
         if (combine) {
             found = true;
             for (var i = 0; i < showit.length; ++i) {
@@ -72,41 +72,41 @@ function update_filter(e) {
                 }
             }
         }
-		if (found) {
-			elems[idx].style.display = "";
-			continue;
-		}
-		if (showit.length == 0) {
-			elems[idx].style.display = "";
-		} else {
-			elems[idx].style.display = "none";
-		}
-	}
+        if (found) {
+            elems[idx].classList.remove("hideme");
+            continue;
+        }
+        if (showit.length == 0) {
+            elems[idx].classList.remove("hideme");
+        } else {
+            elems[idx].classList.add("hideme");
+        }
+    }
 }
 
 
 function createCookie(name, value, days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
 }
 
 function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
 }
 
 function eraseCookie(name) {
-	createCookie(name,"",-1);
+    createCookie(name,"",-1);
 }
 
